ef166543647111a1dab7372441f93cf498ed43fb
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-11-28 10:34:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function volume_sphere() {
     //Write your code here
+	 const radius = parseFloat(document.getElementById('radius').value);
+
+      // Calculate the volume of the sphere
+      const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
+      // Display the result
+      alert(`The volume of the sphere is: ${volume.toFixed(2)}`);
+      
+      // Prevent the form from submitting and refreshing the page
+      return false
   
 } 
 
